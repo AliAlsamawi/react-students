0918983a94fd0614db0c928efb2ecc93294b9549
Ali A <alsamawiali4@gmail.com>
2021-12-06 15:15:04 -0500

    done

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Student from './Student';
+import studentsdata from './data.js';
+
+function App() {
+  const students = studentsdata.map((ele, index) => {
+    return(
+      <Student
+        name={ele.name}
+        bio={ele.bio}
+        scores={ele.scores}
+        key={index}
+      />
+    )
+  })
+  
+  return (
+    <div className="App">
+      {students}
+    </div>
+  );
+}
+
+export default App;
